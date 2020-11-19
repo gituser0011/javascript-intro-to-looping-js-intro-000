2fefb585f40f15bc791d94fcbc92f6d68a7528f3
S  1 <lokohopo@gmail.com>
2020-11-19 21:48:13 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -11,12 +11,22 @@ function forLoop(array) {
 }
 
 function whileLoop(n) {
-  let countdown = n;
+	while (n > 0) {
+		console.log(--n);
+	}
 
-    while (countdown > 0) {
-      console.log(--countdown)
-    }
-    return "done";
-  }
+	return 'done';
+}
 
 function doWhileLoop(num) {
+	var i = 0;
+
+	function incrementVariable() {
+		i = i + 1;
+    return i;
+	}
+
+  do {
+    console.log("I run once regardless.");
+  } while (incrementVariable() < num);
+}
