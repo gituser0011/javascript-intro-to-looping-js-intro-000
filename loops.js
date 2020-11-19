9a4b83c4890716f5bf87940bbe8ad41146a9f86b
S  1 <lokohopo@gmail.com>
2020-11-19 21:50:52 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,10 +1,15 @@
 function forLoop(array) {
 	for (let i = 0; i < 25; i++) {
-	array.push (`I am ${i} strange loop${i === 0 ? '' : 's'}.`)
+		if (i === 1) {
+			array.push('I am 1 strange loop.');
+		} else {
+			array.push(`I am ${i} strange loops.`);
 		}
-    return array
 	}
+
+	return array;
 }
+
 
 function whileLoop(n) {
 	while (n > 0) {
